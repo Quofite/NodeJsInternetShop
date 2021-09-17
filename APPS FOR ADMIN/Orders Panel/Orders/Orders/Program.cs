7a440ea0a4692a8d97fddfb4d0fab4b02a41795c
Quofite <gleb.n1kit1n@yandex.ru>
2021-09-17 11:16:23 +0300

    ver 0.31 - made autoupdating system for op

--- APPS FOR ADMIN/Orders Panel/Orders/Orders/Program.cs
+++ APPS FOR ADMIN/Orders Panel/Orders/Orders/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Data.Common;
 using MySql.Data.MySqlClient;
 
@@ -6,31 +7,14 @@ namespace Orders
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            MySqlConnection conn = DBUtils.GetDBConnection();   // getting connection, that creates in Class "DBUtils" and in Function "GetDBConnection"
-            conn.Open();                                        // opening connection
+        static void Main(string[] args) {
+            TimerCallback tc = new TimerCallback(GetConnection);    // creating delegate object for timer
+            Timer timer = new Timer(tc, null, 0, 300000);            // creating timer. args: callback, value to send in callback, delay to start timer, delay to call function again
 
-            try
-            {
-                GetOrders(conn);    // getting data from db
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: " + e);   // showing error if it excists
-                Console.WriteLine(e.StackTrace);
-            }
-            finally {
-                conn.Close();   // closing connection after all
-                conn.Dispose(); // deliting connection object to free ram
-            }
-
-            Console.WriteLine("Press any key to stop the application");
-            Console.ReadKey();
+            Console.ReadKey();      // needs to make app don't close immediately
         }
 
-        private static void GetOrders(MySqlConnection connection)
-        {
+        private static void GetOrders(MySqlConnection connection) {
             string sql = "SELECT * FROM `orders`";  // sql-string to get everything form table named 'orders'
 
             // getting ability to use sql commands and send request
@@ -50,7 +34,7 @@ namespace Orders
                         string phone = reader.GetString(3);
                         string carId = reader.GetString(4);
 
-                        Console.WriteLine("---------------");   // amazing border
+                        Console.WriteLine("-------------------------------");   // amazing border
                         Console.WriteLine("Order ID: " + orderId);
                         Console.WriteLine("Buyer Name: " + buyerName);
                         Console.WriteLine("Buyer's address: " + address);
@@ -58,6 +42,22 @@ namespace Orders
                         Console.WriteLine("Ordered car ID: " + carId);
                     }
                 }
+            }
+        }
+
+        public static void GetConnection(object obj) {  // have to have this arg, for some reasons imposible to make callback without it, but in timer just setting "null" in it
+            Console.Clear();
+            MySqlConnection conn = DBUtils.GetDBConnection();   // getting connection, that creates in Class "DBUtils" and in Function "GetDBConnection"
+            conn.Open();                                        // opening connection
+
+            try {
+                GetOrders(conn);    // getting data from db
+            } catch (Exception e) {
+                Console.WriteLine("Error: " + e);   // showing error if it excists
+                Console.WriteLine(e.StackTrace);
+            } finally {
+                conn.Close();   // closing connection after all
+                conn.Dispose(); // deleting connection object to free ram
             }
         }
     }
